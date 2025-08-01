1a67b437a0c2b6b1d375fbec91bf53de6de67cfe
Jennesy Dai <jennesy.dai@gmail.com>
2025-08-01 10:06:06 +0800

    fix: description

--- src/components/transfer-instructions-panel.vue
+++ src/components/transfer-instructions-panel.vue
@@ -6,21 +6,20 @@
     <div class='flex flex-col gap-6'>
       <div class='flex gap-2 items-start'>
         <div class='w-7 mobile:w-8 aspect-square bg-gray-600 text-white rounded-full flex items-center justify-center text-lg mobile:text-xl font-bold flex-shrink-0 '> 1 </div>
-        <div class='mobile:text-lg'>前往<a href='https://docs.google.com/spreadsheets/d/1b9FVc7L0r_NvNOFQJsuUSoazTQKjKubuInqlOjRiM0c/edit?usp=sharing' target='_blank'>
+        <div class='mobile:text-lg'>到<a href='https://docs.google.com/spreadsheets/d/1b9FVc7L0r_NvNOFQJsuUSoazTQKjKubuInqlOjRiM0c/edit?usp=sharing' target='_blank'>
             <span class="inline-flex items-center translate-y-1">
               <v-icon>mdi-google-spreadsheet</v-icon>
               <div>訂單列表</div>
             </span>
           </a>
-          ，確認 
-          <span class='font-bold border-b-solid'>訂單編碼</span> 與 
-          <span class='font-bold border-b-solid'>金額</span>
+          ，找到自己的
+          <span class='font-bold border-b-solid'>訂單號碼</span> 與 <span class='font-bold border-b-solid'>金額</span>
         </div>
       </div>
       <div class='flex gap-2 items-start'>
         <div class='w-7 mobile:w-8 aspect-square bg-gray-600 text-white rounded-full flex items-center justify-center text-lg mobile:text-xl font-bold flex-shrink-0 '> 2 </div>
         <div class='flex flex-col mobile:text-lg'>
-          <div class='mb-2'>匯款，並在備著寫上<span class='font-bold border-b-solid'>訂單編碼</span></div>
+          <div class='mb-2'>完成匯款後，請在備註欄位寫上<span class='font-bold border-b-solid'>訂單號碼</span></div>
           <div class='px-6 py-2 bg-gray-100 rounded-lg'>
             <div class='text-base'>銀行代碼：004</div>
             <div class='text-base'>分行代碼：民生 0808</div>
