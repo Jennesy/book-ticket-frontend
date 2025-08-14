ddcb50bb52def55430c57e16384935280772f097
Jennesy Dai <jennesy.dai@gmail.com>
2025-08-14 10:06:05 +0200

    feat: move transfer info to .env

--- src/components/transfer-instructions-panel.vue
+++ src/components/transfer-instructions-panel.vue
@@ -6,7 +6,7 @@
     <div class='flex flex-col gap-6'>
       <div class='flex gap-2 items-start'>
         <div class='w-7 mobile:w-8 aspect-square bg-gray-600 text-white rounded-full flex items-center justify-center text-lg mobile:text-xl font-bold flex-shrink-0 '> 1 </div>
-        <div class='mobile:text-lg'>到<a href='https://docs.google.com/spreadsheets/d/1b9FVc7L0r_NvNOFQJsuUSoazTQKjKubuInqlOjRiM0c/edit?usp=sharing' target='_blank'>
+        <div class='mobile:text-lg'>到<a :href='orderListUrl' target='_blank'>
             <span class="inline-flex items-center translate-y-1">
               <v-icon>mdi-google-spreadsheet</v-icon>
               <div>訂單列表</div>
@@ -21,10 +21,10 @@
         <div class='flex flex-col mobile:text-lg'>
           <div class='mb-2'>完成匯款後，請在備註欄位寫上<span class='font-bold border-b-solid'>訂單號碼</span></div>
           <div class='px-6 py-2 bg-gray-100 rounded-lg'>
-            <div class='text-base'>銀行代碼：永豐銀行 807</div>
-            <div class='text-base'>分行代碼：德惠分行 1583</div>
-            <div class='text-base'>銀行帳號：{{ accountNumber }} <v-icon size='16' class='cursor-pointer hover:bg-gray-200 rounded-full' @click='onCopy'>mdi-content-copy</v-icon></div>
-            <div class='text-base'>戶名：張雅筑</div>
+            <div class='text-base'>銀行代碼：{{ transferAccount.bank }}</div>
+            <div class='text-base'>分行代碼：{{ transferAccount.branch }}</div>
+            <div class='text-base'>銀行帳號：{{ transferAccount.accountNumber }} <v-icon size='16' class='cursor-pointer hover:bg-gray-200 rounded-full' @click='onCopy'>mdi-content-copy</v-icon></div>
+            <div class='text-base'>戶名：{{ transferAccount.accountName }}</div>
           </div>
         </div>
       </div>
@@ -41,9 +41,15 @@
 
   const { isMobile } = useWindowSize()
   const { copyToClipboard } = useCopyToClipboard()
-  const accountNumber = '08801800387632'
+  const orderListUrl = import.meta.env.VITE_ORDER_LIST_URL
+  const transferAccount = {
+    bank: import.meta.env.VITE_TRANSFER_BANK_NAME,
+    branch: import.meta.env.VITE_TRANSFER_BANK_BRANCH,
+    accountNumber: import.meta.env.VITE_TRANSFER_BANK_ACCOUNT_NUMBER,
+    accountName: import.meta.env.VITE_TRANSFER_BANK_ACCOUNT_OWNER
+  }
 
   const onCopy = () => {
-    copyToClipboard(accountNumber, '帳號已複製到剪貼簿')
+    copyToClipboard(transferAccount.accountNumber, '帳號已複製到剪貼簿')
   }
 </script>
