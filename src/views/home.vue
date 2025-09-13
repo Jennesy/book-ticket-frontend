8b9bb7e847b04e9c5b1668daf52fdcbe2ba62e79
Jennesy Dai <jennesy.dai@gmail.com>
2025-09-14 00:19:58 +0800

    feat: add to booking page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class='flex items-center justify-center text-size-5.5 gap-2 translate-x-4 text-gray-600 font-bold mb-4'>
+  <div class='flex items-center justify-center text-size-5.5 gap-2 translate-x-4 text-gray-600 font-bold -mb-3'>
     <div>座位圖</div>
     <v-icon size='22' class='!text-gray-600 !hover:text-gray-900 cursor-pointer absolute top-0 right-0' @click='onInfoPanelOpen'>mdi-information</v-icon>
+  </div>
+  <div class='flex items-center justify-end text-indigo-600 hover:text-indigo-900 cursor-pointer -translate-x-2 translate-y-1 text-sm' @click='onToBookingClick'>前往購票
+    <v-icon size='18'>mdi-arrow-right</v-icon>
   </div>
   <SeatMap />
   <InfoPanel v-if="isInfoPanelVisible" @close="onInfoPanelClose" />
@@ -9,6 +12,7 @@
 
 <script>
   import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
   import SeatMap from '@/components/seat-map'
   import InfoPanel from '@/components/info-panel'
 
@@ -19,14 +23,17 @@
     },
     setup() {
       const isInfoPanelVisible = ref(false)
-
+      const router = useRouter()
       const onInfoPanelOpen = () => isInfoPanelVisible.value = true
       const onInfoPanelClose = () => isInfoPanelVisible.value = false
+
+      const onToBookingClick = () => router.push('/booking')
 
       return {
         isInfoPanelVisible,
         onInfoPanelOpen,
         onInfoPanelClose,
+        onToBookingClick,
       }
     }
   }
